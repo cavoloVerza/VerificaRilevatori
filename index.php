20f48f159bef24e359504a4fb79b8f0578aaa685
lai2-24 <lai2-24@itismeucci.com>
2024-03-13 11:21:58 +0100

    creata repo

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+
+    use Slim\Factory\AppFactory;
+
+    require __DIR__ . '/vendor/autoload.php';
+
+    spl_autoload_register(function($class){
+        
+        if(file_exists("./models/$class.php"))
+            require_once("./models/$class.php");
+
+        else if(file_exists("./controllers/$class.php"))
+            require_once("./controllers/$class.php");
+
+    });
+
+
+    $app = AppFactory::create();
+    
+
+    $app->get('/', 'SiteController:index');
+    $app->get('/alunni', 'AlunniController:alunni');
+    $app->get('/alunni/{nome}', 'AlunniController:search');
+
+    $app->get('/api/alunni', 'ApiAlunniController:alunni');
+    $app->get('/api/alunni/{nome}', 'ApiAlunniController:search');
+
+    $app->post('/api/alunni', 'ApiAlunniController:createAlunno');
+    $app->put('/api/alunni/{nome}', 'ApiAlunniController:updateAlunno');
+    $app->delete('/api/alunni/{nome}', 'ApiAlunniController:deleteAlunno');
+
+    $app->run();
+
+?>
